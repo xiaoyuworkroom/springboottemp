f8ed9fa7b6cdc1dbf59a83fca182af1d28997e84
xiaoyu <xiaoyu.li9966@gmail.com>
2019-06-17 07:21:11 +0800

    增加controller 函数

--- src/main/java/com/xiaoyu/springbootdemo/controller/MyController.java
+++ src/main/java/com/xiaoyu/springbootdemo/controller/MyController.java
@@ -1,6 +1,7 @@
-package com.xiaoyu.springbootdemo;
+package com.xiaoyu.springbootdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,12 @@ public class MyController {
     @GetMapping("hello")
     public String hello(){
         return "hello world!";
+    }
+
+
+    @GetMapping("md5/{filePath}")
+    public String GetMD51(@PathVariable String filePath){
+
+        return "filePath111: " + filePath;
     }
 }
