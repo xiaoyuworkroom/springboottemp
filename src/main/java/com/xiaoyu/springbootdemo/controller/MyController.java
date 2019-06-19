e107d4326e13e62dc963d6b09a2f31d9b9dee2b7
xiaoyu <xiaoyu.li9966@gmail.com>
2019-06-20 07:26:56 +0800

    李晓宇: 测试 获取服务器地址

--- src/main/java/com/xiaoyu/springbootdemo/controller/MyController.java
+++ src/main/java/com/xiaoyu/springbootdemo/controller/MyController.java
@@ -1,8 +1,13 @@
 package com.xiaoyu.springbootdemo.controller;
 
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MyController {
@@ -15,7 +20,21 @@ public class MyController {
 
     @GetMapping("md5/{filePath}")
     public String GetMD51(@PathVariable String filePath){
-
         return "filePath111: " + filePath;
+    }
+
+    @GetMapping("servletPath")
+    public String servletPath(){
+
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        //String ret = request.getContextPath();
+
+        //ClassUtils.getDefaultClassLoader().getResource("").getPath();
+
+        String ret = new ApplicationHome(this.getClass()).getSource().getParentFile().getParentFile().getPath();
+//        String ret = request.getServletPath();
+        return ret;
     }
 }
